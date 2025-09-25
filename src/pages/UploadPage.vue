d2a8fd1752fd7d6238d3a4d893df6a9e08f9ffed
justin2hu <183563054+justin21172tw@users.noreply.github.com>
2025-09-25 22:25:55 +0800

    update UploadPage layuot

--- src/pages/UploadPage.vue
+++ src/pages/UploadPage.vue
@@ -1,68 +1,76 @@
 <template>
-  <q-page class="q-pa-md flex column items-center">
-    <q-card flat bordered class="q-pa-lg q-mx-auto" style="max-width: 600px; width: 100%">
-      <q-card-section class="flex flex-center">
-        <br />
-        <q-img
-          src="../../public/icons/CKHS-LOGO.png"
-          alt="cklogo"
-          style="max-width: 50%; max-height: 50%"
-          fit="contain"
-        />
-      </q-card-section>
-      <q-card-section class="flex flex-center">
-        <h4 class="text-center"><strong>社團教師資料上傳</strong></h4>
+  <q-page class="q-pa-md">
+    <q-card flat bordered class="q-pa-lg q-mx-auto" style="max-width: 800px; width: 100%">
+      <!-- 標題 -->
+      <q-card-section class="text-center">
+        <h4><strong>社團教師資料上傳</strong></h4>
       </q-card-section>
 
       <q-separator spaced />
 
+      <!-- 兩欄佈局：A.校內 和 B.校外 -->
       <q-card-section>
-        <div class="q-pb-xs">
-          <div class="text-center" style="font-size: x-large">
-            <strong>請詳閱說明</strong><br /><br />
+        <div class="row q-col-gutter-lg">
+          <!-- A. 校內指導老師 -->
+          <div class="col-12 col-md-6">
+            <q-card flat bordered class="full-height q-pa-md">
+              <div class="text-h6 text-weight-bold text-center q-mb-md">A. 校內指導老師</div>
+              <div class="text-center">不須繳交任何文件</div>
+            </q-card>
           </div>
-          <strong>A. 校內指導老師</strong><br />
-          不須繳交任何文件
+
+          <!-- B. 外聘指導老師 -->
+          <div class="col-12 col-md-6">
+            <q-card flat bordered class="full-height q-pa-md">
+              <div class="text-h6 text-weight-bold text-center q-mb-md">B. 外聘指導老師</div>
+              <div class="q-mb-sm">
+                <strong>1.</strong> 所有外聘老師：每年皆須繳交新的會辦單+契約書
+              </div>
+              <div><strong>2.</strong> 新進外聘老師：需另外繳交一次資料卡</div>
+            </q-card>
+          </div>
         </div>
-        <br />
-        <div class="q-pb-xs">
-          <strong>B. 外聘指導老師</strong><br />
-          1. 所有外聘老師：每年皆須繳交新的會辦單+契約書<br />
-          2. 新進外聘老師：需另外繳交一次資料卡
-        </div>
-        <br />
-        <br />
-        <div class="row justify-center">
-          <q-btn
-            href="https://docs.google.com/forms/d/e/1FAIpQLSf5Nwy5x4QQjvYkBAi_7cEWRflToVx5JqFpQ2V8DBeUINaLyg/viewform"
-            style="color: white; background-color: #1c2087"
-            class="q-mx-auto"
-            target="_blank"
-          >
-            資料繳交表單
-          </q-btn>
-        </div>
-        <br />
-        <div class="row justify-center">
-          <q-btn
-            href="https://sites.google.com/gl.ck.tp.edu.tw/ckclub/%E7%A4%BE%E5%9C%98%E6%95%99%E5%B8%AB%E8%B3%87%E6%96%99%E4%B8%8A%E5%82%B3?authuser=0#h.bwe8631n56yd"
-            style="color: white; background-color: #1c2087"
-            class="q-mx-auto"
-            target="_blank"
-          >
-            會辦單、契約書
-          </q-btn>
-        </div>
-        <br />
-        <div class="row justify-center">
-          <q-btn
-            href="https://sites.google.com/gl.ck.tp.edu.tw/ckclub/%E7%A4%BE%E5%9C%98%E6%95%99%E5%B8%AB%E8%B3%87%E6%96%99%E4%B8%8A%E5%82%B3?authuser=0#h.cg1ohytd944b"
-            style="color: white; background-color: #1c2087"
-            class="q-mx-auto"
-            target="_blank"
-          >
-            資料卡
-          </q-btn>
+      </q-card-section>
+
+      <!-- 按鈕區域 - 左側四分之一處 -->
+      <q-card-section>
+        <div class="row">
+          <div class="col-3">
+            <div class="column q-gutter-md">
+              <q-btn
+                href="https://docs.google.com/forms/d/e/1FAIpQLSf5Nwy5x4QQjvYkBAi_7cEWRflToVx5JqFpQ2V8DBeUINaLyg/viewform"
+                style="color: white; background-color: #1c2087"
+                target="_blank"
+                size="md"
+                class="full-width"
+              >
+                1. 資料繳交表單
+              </q-btn>
+
+              <q-btn
+                href="https://sites.google.com/gl.ck.tp.edu.tw/ckclub/%E7%A4%BE%E5%9C%98%E6%95%99%E5%B8%AB%E8%B3%87%E6%96%99%E4%B8%8A%E5%82%B3?authuser=0#h.bwe8631n56yd"
+                style="color: white; background-color: #1c2087"
+                target="_blank"
+                size="md"
+                class="full-width"
+              >
+                2. 會辦單、契約書
+              </q-btn>
+
+              <q-btn
+                href="https://sites.google.com/gl.ck.tp.edu.tw/ckclub/%E7%A4%BE%E5%9C%98%E6%95%99%E5%B8%AB%E8%B3%87%E6%96%99%E4%B8%8A%E5%82%B3?authuser=0#h.cg1ohytd944b"
+                style="color: white; background-color: #1c2087"
+                target="_blank"
+                size="md"
+                class="full-width"
+              >
+                3. 資料卡
+              </q-btn>
+            </div>
+          </div>
+          <div class="col-9">
+            <!-- 右側空白區域 -->
+          </div>
         </div>
       </q-card-section>
     </q-card>
